7a2b6502f61779bcde461ac8952edfe613895015
kimkohlenbach <kim.kohlenbach@digitaldesk.com.br>
2018-02-05 14:36:33 -0200

    Lógica de sortear nomes femininos juntamente com o layout

--- app/src/main/java/com/example/kimamanda/sorteadornomes/MainActivity.java
+++ app/src/main/java/com/example/kimamanda/sorteadornomes/MainActivity.java
@@ -2,12 +2,81 @@ package com.example.kimamanda.sorteadornomes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.List;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView texto;
+    Button feminino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        texto = (TextView) findViewById(R.id.texto);
+        feminino = (Button) findViewById(R.id.feminino);
+
+    }
+
+    public class Nomes {
+
+        public void main(String[] args) {
+
+            ArrayList<String> Femininos = new ArrayList<String>();
+
+            //adiciona itens na lista
+            Femininos.add("Kim");
+            Femininos.add("Millena");
+            Femininos.add("Bianca");
+            Femininos.add("Ana");
+            Femininos.add("Nicole");
+            Femininos.add("Julia");
+            Femininos.add("Alice");
+            Femininos.add("Sofia");
+            Femininos.add("Carolina");
+            Femininos.add("Sabrina");
+            Femininos.add("Manuela");
+
+            int i;
+            Random r = new Random();
+            int n = Femininos.size();
+            for (i = 0; i < 1; i++) {
+                //System.out.println(Femininos.get(r.nextInt(Femininos.size())));
+
+            }
+        }
+    }
+
+    public void mostra(View v) {
+
+        ArrayList<String> Femininos = new ArrayList<String>();
+        Femininos.add("Kim");
+        Femininos.add("Millena");
+        Femininos.add("Bianca");
+        Femininos.add("Ana");
+        Femininos.add("Nicole");
+        Femininos.add("Julia");
+        Femininos.add("Alice");
+        Femininos.add("Sofia");
+        Femininos.add("Carolina");
+        Femininos.add("Sabrina");
+        Femininos.add("Manuela");
+
+        int i;
+        Random r = new Random();
+        int n = Femininos.size();
+        for (i = 0; i < 1; i++) {
+
+            texto.setText(Femininos.get(r.nextInt(Femininos.size())));
+        }
     }
 }
+
