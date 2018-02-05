54c6af41953426a50d3d015984ae0e179667378e
kimkohlenbach <kim.kohlenbach@digitaldesk.com.br>
2018-02-05 15:20:09 -0200

    Adição sorteador nomes masculinos e formatação no layout

--- app/src/main/java/com/example/kimamanda/sorteadornomes/MainActivity.java
+++ app/src/main/java/com/example/kimamanda/sorteadornomes/MainActivity.java
@@ -9,20 +9,25 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.List;
+
 import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView texto;
+    Button masculino;
     Button feminino;
+    Button limpa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        limpa = (Button)  findViewById(R.id.limpa);
         texto = (TextView) findViewById(R.id.texto);
         feminino = (Button) findViewById(R.id.feminino);
+        masculino = (Button) findViewById(R.id.masculino);
 
     }
 
@@ -31,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
         public void main(String[] args) {
 
             ArrayList<String> Femininos = new ArrayList<String>();
-
-            //adiciona itens na lista
             Femininos.add("Kim");
             Femininos.add("Millena");
             Femininos.add("Bianca");
@@ -45,17 +48,25 @@ public class MainActivity extends AppCompatActivity {
             Femininos.add("Sabrina");
             Femininos.add("Manuela");
 
-            int i;
-            Random r = new Random();
-            int n = Femininos.size();
-            for (i = 0; i < 1; i++) {
-                //System.out.println(Femininos.get(r.nextInt(Femininos.size())));
+            ArrayList<String> Masculinos = new ArrayList<String>();
+            Masculinos.add("Marcos");
+            Masculinos.add("Lucas");
+            Masculinos.add("Thiago");
+            Masculinos.add("Gustavo");
+            Masculinos.add("Geison");
+            Masculinos.add("Vítor");
+            Masculinos.add("Caio");
+            Masculinos.add("Vinícius");
+            Masculinos.add("Carlos");
+            Masculinos.add("Nicolas");
+            Masculinos.add("Pedro");
 
-            }
         }
+
+
     }
 
-    public void mostra(View v) {
+    public void mostra_feminino(View v) {
 
         ArrayList<String> Femininos = new ArrayList<String>();
         Femininos.add("Kim");
@@ -77,6 +88,34 @@ public class MainActivity extends AppCompatActivity {
 
             texto.setText(Femininos.get(r.nextInt(Femininos.size())));
         }
+    }
+
+    public void mostra_masculino (View v) {
+
+        ArrayList<String> Masculinos = new ArrayList<String>();
+        Masculinos.add("Marcos");
+        Masculinos.add("Lucas");
+        Masculinos.add("Thiago");
+        Masculinos.add("Gustavo");
+        Masculinos.add("Geison");
+        Masculinos.add("Vítor");
+        Masculinos.add("Caio");
+        Masculinos.add("Vinícius");
+        Masculinos.add("Carlos");
+        Masculinos.add("Nicolas");
+        Masculinos.add("Pedro");
+
+        int i;
+        Random r = new Random();
+        int n = Masculinos.size();
+        for (i = 0; i < 1; i++) {
+
+            texto.setText(Masculinos.get(r.nextInt(Masculinos.size())));
+        }
+    }
+
+    public void limpar (View v){
+        texto.setText("");
     }
 }
 
